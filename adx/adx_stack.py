0d8d74c1f201a221d8d931c7bfcbff3b0e0a6c93
EC2 Default User <ec2-user@ip-172-31-92-75.ec2.internal>
2022-09-27 19:15:29 +0000

    initial version

--- adx/adx_stack.py
+++ adx/adx_stack.py
@@ -1,19 +1,76 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    Aws,
+    ArnFormat
 )
+
+from aws_cdk.aws_iam import (
+    ManagedPolicy,
+    PolicyStatement,
+)
+
 from constructs import Construct
+
+from aws_analytics_reference_architecture import (
+        EmrEksCluster,
+        NotebookPlatform,
+        StudioAuthMode,
+        SSOIdentityType,
+        NotebookUserOptions, 
+        NotebookManagedEndpointOptions,
+)
+
 
 class AdxStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        emr_eks = EmrEksCluster.get_or_create(self,
+        eks_admin_role_arn="arn:aws:iam::318535617973:role/AdminAccess",
+        eks_cluster_name="clusteradx"
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AdxQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        
+        notebook_platform = NotebookPlatform(self, "platform-notebook",
+            emr_eks=emr_eks,
+            eks_namespace="platformns",
+            studio_name="platform",
+            studio_auth_mode=StudioAuthMode.SSO
+        )
+
+        # If the S3 bucket is encrypted, add policy to the key for the role
+        policy1 = ManagedPolicy(self, "MyPolicy1",
+            statements=[
+                PolicyStatement(
+                    resources=["arn:aws:s3:::alexvt-emr-eks","arn:aws:s3:::clusteradx-emr-eks-assets-318535617973-us-east-1"] ,
+                    actions=["s3:*"]
+                ),
+                PolicyStatement(
+                    resources=[
+                        Stack.format_arn(self,
+                            account=Aws.ACCOUNT_ID,
+                            region=Aws.REGION,
+                            service="logs",
+                            resource="*",
+                            arn_format=ArnFormat.NO_RESOURCE_NAME
+                        )
+                    ],
+                    actions=["logs:*"]
+                )
+            ]
+        )
+        
+        
+        
+        notebook_platform.add_user( 
+            [NotebookUserOptions(
+                identity_name="emrstudio", # make sure user already setup in AWS SSO 
+                identity_type= SSOIdentityType.USER.value,
+                notebook_managed_endpoints = [NotebookManagedEndpointOptions(
+                    emr_on_eks_version = "emr-6.7.0-latest",
+                    managed_endpoint_name = "ManagedEndpoint1",
+                    execution_policy = policy1
+                )]
+            )]
+        )

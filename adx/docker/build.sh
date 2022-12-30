27d36a274a1b86ff09d95f94881289d98557c99f
EC2 Default User <ec2-user@ip-172-31-26-69.ec2.internal>
2022-12-30 14:47:59 +0000

    uncomment array declaration

--- adx/docker/build.sh
+++ adx/docker/build.sh
@@ -14,7 +14,7 @@ REGION=$2
 
 
 #full list https://docs.aws.amazon.com/emr/latest/EMR-on-EKS-DevelopmentGuide/docker-custom-images-tag.html
-#declare -A EMR=( [ap-northeast-1]=059004520145 [ap-northeast-2]=996579266876 [ap-south-1]=235914868574 [ap-southeast-1]=671219180197 [ap-southeast-2]=038297999601 [ca-central-1]=351826393999 [eu-central-1]=107292555468 [us-east-1]=755674844232 [us-east-2]=711395599931 [us-west-1]=608033475327 [us-west-2]=895885662937 )
+declare -A EMR=( [ap-northeast-1]=059004520145 [ap-northeast-2]=996579266876 [ap-south-1]=235914868574 [ap-southeast-1]=671219180197 [ap-southeast-2]=038297999601 [ca-central-1]=351826393999 [eu-central-1]=107292555468 [us-east-1]=755674844232 [us-east-2]=711395599931 [us-west-1]=608033475327 [us-west-2]=895885662937 )
 
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin ${EMR[$REGION]}.dkr.ecr.$REGION.amazonaws.com

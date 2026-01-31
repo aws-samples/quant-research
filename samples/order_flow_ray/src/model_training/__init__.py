8a5d37583f6b20295b8b863e7f7cc5949dc52c03
Alex Tarasov <alexvt@amazon.co.uk>
2026-01-31 18:47:41 +0000

    chronos2 v0.1

--- samples/order_flow_ray/src/model_training/__init__.py
+++ samples/order_flow_ray/src/model_training/__init__.py
@@ -2,9 +2,11 @@
 from .base import Trainer
 from .factory import TrainerFactory
 from .xgboost_trainer import XGBoostTrainer
+from .chronos_trainer import ChronosTrainer
 
 __all__ = [
     'Trainer',
     'TrainerFactory',
     'XGBoostTrainer',
+    'ChronosTrainer',
 ]
--- samples/order_flow_ray/src/model_training/factory.py
+++ samples/order_flow_ray/src/model_training/factory.py
@@ -7,16 +7,19 @@ class TrainerFactory:
     @staticmethod
     def create(trainer_type: str, **kwargs):
         """Create trainer instance.
-        
+
         Args:
             trainer_type: Type of trainer
             **kwargs: Additional arguments for the trainer
-            
+
         Returns:
             Trainer instance
         """
         if trainer_type == 'xgboost':
             from .xgboost_trainer import XGBoostTrainer
             return XGBoostTrainer(**kwargs)
+        elif trainer_type == 'chronos2':
+            from .chronos_trainer import ChronosTrainer
+            return ChronosTrainer(**kwargs)
         else:
             raise ValueError(f"Unknown trainer type: {trainer_type}")
